a422d51057e7d63cc0e52dc19163ffcba6aa81ac
Nicolas Alvarado <naav972@gmail.com>
2024-07-06 22:14:30 -0500

    Elixir init

---
+++ elixir/README.md
@@ -0,0 +1,5 @@
+# Elixir
+
+## How to run:
+
+`elixir <file-name>`
---
+++ elixir/string-contains-substring.exs
@@ -0,0 +1,10 @@
+string = "Fvnjailhgdi"
+IO.puts(string)
+subString = "jail"
+IO.puts(subString)
+
+if String.contains?(string, subString) do
+  IO.puts(true)
+else
+  IO.puts(false)
+end
